51dc65114b0c61a7784e9af3ca72d4b5420cff58
usman8232 <muw.design123@gmail.com>
2025-02-13 13:42:49 +0500

    Add add App.java

--- src/main/java/com/houarizegai/calculator/App.java
+++ src/main/java/com/houarizegai/calculator/App.java
@@ -8,3 +8,20 @@ public class App {
         new CalculatorUI();
     }
 }
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter first number: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        double sum = num1 + num2;
+
+        Console.WriteLine($"The sum is: {sum}");
+    }
+}
